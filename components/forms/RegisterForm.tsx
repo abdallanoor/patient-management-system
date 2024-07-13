44dbb7adbe424a27d94ec5e3f7bcfe3c5e534080
Abdallah <abdallaahnoor@gmail.com>
2024-07-13 15:31:03 +0300

    edit register form

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -91,6 +91,8 @@ const RegisterForm = ({ user }: { user: User }) => {
           ? formData
           : undefined,
         privacyConsent: values.privacyConsent,
+        treatmentConsent: values.treatmentConsent,
+        disclosureConsent: values.disclosureConsent,
       };
 
       const newPatient = await registerPatient(patient);
